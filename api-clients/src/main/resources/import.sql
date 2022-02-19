b2f89f5054f055abeb09b074c3c69f29b0568e5c
TomasAlric <tomas.alricc@gmail.com>
2022-02-19 05:01:21 -0300

    Criando banco de dados seeding

---
+++ api-clients/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vitória Gonçalves', '298.287.590-04', 5545.35, TIMESTAMP WITH TIME ZONE '1998-01-31T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marlon Silva', '881.556.070-03', 3278.20, TIMESTAMP WITH TIME ZONE '1996-12-20T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Paulo Lopes', '052.894.400-20', 4742.15, TIMESTAMP WITH TIME ZONE '1992-06-10T20:50:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruno Miranda', '489.918.000-41', 2756.54, TIMESTAMP WITH TIME ZONE '1991-04-13T20:50:07.12345Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Laura Ramalho', '696.208.000-01', 1873.60, TIMESTAMP WITH TIME ZONE '1993-03-16T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Michelli Britto', '570.403.520-52', 9858.80, TIMESTAMP WITH TIME ZONE '1990-08-19T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Venâncio', '986.374.220-12', 8300.85, TIMESTAMP WITH TIME ZONE '1984-01-21T20:50:07.12345Z', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Jaqueline Cassemiro', '853.204.130-25', 3634.10, TIMESTAMP WITH TIME ZONE '1952-03-30T20:50:07.12345Z', 8);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('André Marques', '696.271.180-87', 5393.65, TIMESTAMP WITH TIME ZONE '1986-01-10T20:50:07.12345Z', 9);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Reis', '311.050.060-42', 6350.10, TIMESTAMP WITH TIME ZONE '1983-05-10T20:50:07.12345Z', 5);
