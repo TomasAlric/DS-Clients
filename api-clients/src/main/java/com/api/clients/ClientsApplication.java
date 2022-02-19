ee822ad3736426693c523a940cc47ef77f587777
TomasAlric <tomas.alricc@gmail.com>
2022-02-19 00:16:54 -0300

    Configurando application.properties e ambiente de testes

---
+++ api-clients/src/main/java/com/api/clients/ClientsApplication.java
@@ -0,0 +1,13 @@
+package com.api.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsApplication.class, args);
+	}
+
+}
---
+++ api-clients/src/test/java/com/api/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
